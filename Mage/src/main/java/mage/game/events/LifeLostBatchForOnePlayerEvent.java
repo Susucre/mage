e670032c15b15c7ede25db53998a2d06a8d9bcb8
Susucre <34709007+Susucre@users.noreply.github.com>
2024-05-05 21:07:38 +0200

    fix new event

--- Mage/src/main/java/mage/game/events/LifeLostBatchForOnePlayerEvent.java
+++ Mage/src/main/java/mage/game/events/LifeLostBatchForOnePlayerEvent.java
@@ -6,6 +6,6 @@ package mage.game.events;
 public class LifeLostBatchForOnePlayerEvent extends BatchEvent<LifeLostEvent> {
 
     public LifeLostBatchForOnePlayerEvent(LifeLostEvent firstEvent) {
-        super(EventType.LOST_LIFE_BATCH_FOR_ONE_PLAYER, true, false, firstEvent);
+        super(EventType.LOST_LIFE_BATCH_FOR_ONE_PLAYER, true, false, true, firstEvent);
     }
 }
