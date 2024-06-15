ed4730e5a5a02243c218539c93dcc1e8a743053c
xenohedron <xenohedron@users.noreply.github.com>
2024-06-15 00:26:38 -0400

    fixes

--- Mage/src/main/java/mage/abilities/AbilityImpl.java
+++ Mage/src/main/java/mage/abilities/AbilityImpl.java
@@ -469,6 +469,7 @@ public abstract class AbilityImpl implements Ability {
                 case DISGUISE:
                 case PLOT:
                 case FORETELL:
+                case MUTATE:
                     // from Snapcaster Mage:
                     // If you cast a spell from a graveyard using its flashback ability, you can't pay other alternative costs
                     // (such as that of Foil). (2018-12-07)
